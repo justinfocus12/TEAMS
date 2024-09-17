2146bbec5d95f06f2e7d0e317285b1670b963bf5
ju26596 <ju26596@eofe10.mit.edu>
2024-09-17 19:28:55 -0400

    Began populating cane_zebiak with instructions for implementing the dynamical system

--- examples/cane_zebiak/cane_zebiak.py
+++ examples/cane_zebiak/cane_zebiak.py
@@ -28,12 +28,63 @@ from dynamicalsystem import DynamicalSystem
 import forcing
 
 class CaneZebiak(DynamicalSystem):
+    # TODO for SP: implement concrete methods implementing the abstract methods of DynamicalSystem, as stipulated in "../../dynamicalsystem.py". They are listed below. 
+    def generate_default_icandf(self,init_time,fin_time,seed=None): 
+        """
+        Inputs
+        - init_time: time corresponding to an initial condition (should be an integer, specifying the number of time units since time 0)
+        - fin_time: time (integer) when the integration should finish
+        - seed: integer for random-number generator to create the random force
+
+        Outputs
+        - icandf of the format specified in the inputs to run_trajectory
+
+        Side effects: none (unless needed to calculate outputs). 
+        """
+        pass
+    @staticmethod
+    def get_timespan(metadata):
+        pass
+    @staticmethod
+    def observable_props():
+        # Should return a dictionary whose keys correspond to class methods and whose values correspond to plotting arguments
+        pass
+    def compute_pairwise_observables(self, pair_funs, md0, md1list, root_dir):
+        """
+        Not needed now (helpful for comparison of two trajectories, e.g., Euclidean distance, mean)
+        """
+        return 
+    def compute_observables(self, obs_funs, metadata, root_dir):
+        # obs_names must correspond to class methods
+        """
+        Not needed now (helpful for batch calculation of multiple low-dimensional observables without reading the output files repeatedly)
+        """
+        return
     def run_trajectory(self, icandf, obs_fun, saveinfo, root_dir):
-        # return some metadata sufficient to reconstruct the output, for example (1) a filename, (2) full numpy array of the output of an ODE solver. 
-        # Optionally, return some observables passed as a dictionary of function handles
-        # icandf stands for "initial conditions and forcing." It: could be e.g. 
-        # 1. (a full state vector, a few impulses) (for a small ODESystem) 
-        # 2. (a full state vector, a few reseeds) (for a small stochastic ODESystem) 
-        # 3. (A filename containing a full state vector, a namelist) (for a big PDE system)
-        # All directories specified in icandf and saveinfo must be relative to root_dir
-        pass 
+        """
+        Inputs
+        - icandf (for 'initial conditions and forcing'): dictionary with following items
+            a. init_cond: filename containing the initial condition, e.g., a restart leftover from a spinup run, BEFORE perturbation (because later ensemble members might want to launch from the same file with their own perturbation).
+            b. frc: an object of type CaneZebiakForcing (see the class below). 
+        - obs_fun: an "observable function" that the external calling function might want to use immediately for deciding what to do next, without having to read the cumbersome output files produced by the model. If not needed, just set obs_fun = lambda model_output: None
+        - saveinfo: output filenames, temporary directories, etc.; whatever is needed to specify where to store the output. In case you want to move the whole output directory elsewhere later, I suggest making these paths relative to the final argument, root_dir.
+        - root_dir: the path relative to which saveinfo is specified. 
+
+        Outputs: 
+        - metadata: all info needed to recreate the output (suggested format below). 
+        - observables: result of evaluating obs_fun on the integration.
+
+        Side effects: model gets integrated and saved to the file system. 
+
+        """
+        metadata = dict({
+            'icandf': icandf, 
+            'filename_traj': join(root_dir,saveinfo['filename_traj']),
+            'filename_restart': join(root_dir,saveinfo['filename_restart']),
+            })
+        return metadata, observables
+
+class CaneZebiakForcing(forcing.Forcing):
+    # TODO for SP: implement concrete methods implementing the abstract methods of Forcing, as stipulated in "../../forcing.py". (There's only one: get_forcing_times()). Beyond that, devise a compact data structure that fully specifies the times and shapes of perturbations, which can then be used in CaneZebiak.run_trajectory(). 
+    def get_forcing_times():
+        pass
--- forcing.py
+++ forcing.py
@@ -5,7 +5,7 @@ import numpy as np
 # TODO add plotting methods for each kind of forcing, e.g., overlay the impulse times atop a trajectory timeseries
 
 class Forcing(ABC):
-    def __init__(self, init_time, fin_time, *args, **kwargs):
+    def __init__(self, init_time, fin_time):
         self.init_time = init_time
         self.fin_time = fin_time # inclusive
         return
@@ -58,57 +58,4 @@ class SuperposedForcing(Forcing):
     def get_forcing_times(self):
         return self.forcing_times
 
-
-# Need a composite forcing, a summation of a white noise and impulsive term
-
-#class ImpulsiveForcing(Forcing):
-#    def __init__(self, impulse_times, impulses, fin_time):
-#        super().__init__(impulse_times[0], fin_time)
-#        self.impulse_times = impulse_times
-#        self.impulses = impulses
-#        return
-#    def get_forcing_times(self):
-#        return self.impulse_times
-#    def __str__(self):
-#        s = f'timespan = ({self.init_time},{self.fin_time}); imptimes = {self.impulse_times}'
-#        return s
-#
-#class WhiteNoiseForcing(Forcing):
-#    def __init__(self, reseed_times, seeds, fin_time):
-#        super().__init__(reseed_times[0], fin_time)
-#        self.reseed_times = reseed_times
-#        self.seeds = seeds
-#        return
-#    def get_forcing_times(self):
-#        return self.reseed_times
-#
-#class ContinuousTimeForcing(Forcing):
-#    def __init__(self, init_time, fin_time, reseed_times, seeds):
-#        assert len(reseed_times) == len(seeds)
-#        if len(reseed_times) > 0:
-#            assert (min(reseed_times) >= init_time) and (max(reseed_times) < fin_time)
-#        super().__init__(init_time, fin_time)
-#        self.reseed_times = reseed_times
-#        self.seeds = seeds
-#        return
-#    def get_forcing_times(self):
-#        return self.reseed_times
-#
-#
-#class MultiplicativeTendencyForcing(Forcing):
-#    # Multiply an ODE tendency by a number between 0 and 1
-#    def __init__(self, reseed_times, seeds, fin_time):
-#        super().__init__(reseed_times[0], fin_time)
-#        self.reseed_times = reseed_times
-#        self.seeds = seeds
-#        return
-#    def get_forcing_times(self):
-#        return self.reseed_times
-
-
-
-#class AdditiveTendencyForcing(Forcing):
-#    def __init__(self, init_time, fin_time, params):
-#        super().__init__(init_time, fin_time)
-#        self.
 
